58c7109155000aa02cfaaf198f1688d4ad123d06
Neagoe Ionut <neagoeionut@Ionuts-MacBook-Pro.local>
2017-05-09 13:07:49 +0300

    Front fixes and testing session

--- chat_app_fe/src/components/Main.vue
+++ chat_app_fe/src/components/Main.vue
@@ -1,5 +1,65 @@
 <template>
 	<div class="main">
-		<h1>Main page tbh fam</h1>		
+		<h1>Welcome {{ username }}!</h1>
+		<p>Your userId is: {{ userId }}</p>
+		<p>{{ data }}</p>
+		<input type="button" name="logout-button" value="Logout" @click="logout">	
 	</div>
 </template>
+
+<script>
+import axios from 'axios'
+import router from '../router'
+import session from 'vue-cookie'
+
+export default {
+	name: 'main',
+
+	data() {
+		return {
+			username: '',
+			sessionValid: false,
+			userId: '',
+			data: ''
+		}
+	},
+
+	methods: {
+		logout() {
+	    	const self = this;
+
+	    	axios.get( '/api/user/logout' )
+	    		 .then( ( response ) => {
+	    		 	if ( response.data.resolved ) {
+	    		 		router.push( '/' );
+	    		 	} else {
+	    		 		self.data = router.data.data;
+	    		 		router.push( '/' );
+	    		 	}
+	    		 } );
+	    },
+
+		getSessionInfo() {
+	      const self = this;
+
+	      axios.get( '/api/user/validate' )
+	           .then( ( response ) => {
+	              if ( response.data.resolved ) {
+	                self.sessionValid = true;
+	                self.userId = response.data.userId;
+	                self.username = response.data.username;
+	              } else {
+	              	self.logout();
+	              }
+	           } );
+	    },
+
+	    
+	},
+
+	beforeMount() {
+		this.getSessionInfo();
+	}
+}
+	
+</script>
