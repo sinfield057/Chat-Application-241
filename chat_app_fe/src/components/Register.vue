7c10c1d9a5f7de3a7e0fa3060bdf8520f63626eb
braescurares <braescurares96@gmail.com>
2017-06-07 19:07:02 +0300

    Redesign Register page
    https://trello.com/c/v86pAt3R/11-adding-vuetify-components

--- chat_app_fe/src/components/Register.vue
+++ chat_app_fe/src/components/Register.vue
@@ -1,18 +1,54 @@
 <template>
 	<div class="register">
-		<h1>Register form tbh fam</h1>
-		<p>Account Username:</p>
-		<input type="text" name="username" placeholder="Username" v-model="username">
+		
+		<div class ="data_con">
+			<v-card class="blue-grey lighten-4 elevation-0 pt-5 pb-5">
+			<h3>Register form </h3>	
+				<v-card-text class = "data_register">
+				<v-container fluid>
+					<v-layout row>
+					<v-flex xs8>
+						<v-text-field
+						name="Username"
+							type="text"				
+							label ="Username"
+							class="input-group "
+							v-model="username"
+												
+						></v-text-field>
+					</v-flex>
+					</v-layout>
+					<v-layout row>
+					<v-flex xs8>
+						<v-text-field
+							name="Password"
+							label ="Password"
+							type="password"			
+							class="input-group "
+							v-model="password"	
+							
+						></v-text-field>
+					</v-flex>
+					</v-layout>
+					<v-layout row>
+					<v-flex xs8>
+						<v-text-field
+							name="RepeatPassword"
+							type="password"				
+							label ="RepeatPassword"
+							class="input-group "
+							v-model="passwordRepeat"
+													
+						></v-text-field>
+					</v-flex>
+					</v-layout>        
+				</v-container>
+				</v-card-text>
+				<v-btn  class="blue darken-1 white--text mt-3" name="registe-button" value ="Register" @click.native="doRegister">Register</v-btn>
+			</v-card>			
+		</div>
 		<br />
-		<p>Password</p>
-    	<input type="password" name="password" placeholder="Password" v-model="password">
-    	<br />
-    	<p>Repeat Password</p>
-    	<input type="password" name="password" placeholder="Password" v-model="passwordRepeat">
-    	<br />
-    	<input type="button" name="register-button" value="Register" @click="doRegister">
-    	<br />
-    	<p>{{ data }}</p>
+		<p>{{ data }}</p>
 	</div>
 </template>
 
@@ -74,3 +110,16 @@ export default {
 	}
 }
 </script>
+<style scoped>
+	.data_con{
+		width: 50%;  
+		height: 70%;
+		justify-content: center;
+		margin: auto;
+	
+	}
+    .data-register{
+		justify-content: center;
+		margin: auto;
+	}
+</style>
