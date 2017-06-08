3fffe44b16f9548e91be434364f31b57bc939264
Ionut-Catalin Tatuta <cata_lee_yn@yahoo.com>
2017-06-08 13:43:43 +0300

    Moves link to room chat inside the RoomCard component

--- chat_app_fe/src/components/subcomponents/RoomCard.vue
+++ chat_app_fe/src/components/subcomponents/RoomCard.vue
@@ -1,17 +1,18 @@
 <template>
 	<v-card >
-		<v-card-row class="blue darken-1 white--text mt-3">
-			<v-card-title class="card-title">
-				<v-card-column class="text-xs-left headline ml-2"> {{ room.name }}</v-card-column>
-				<v-card-column class="text-xs-right caption"> Members(TODO)</v-card-column>
-			</v-card-title>
-		</v-card-row>
+		<router-link :to="{ path: 'roomChat/' + room.name }">
+			<v-card-row class="blue darken-1 white--text mt-3">
+				<v-card-title class="card-title">
+					<v-card-column class="text-xs-left headline ml-2"> {{ room.name }}</v-card-column>
+					<v-card-column class="text-xs-right caption"> Members(TODO)</v-card-column>
+				</v-card-title>
+			</v-card-row>
 			<v-card-text>
 				<p class ="text-xs-left subheading">{{ room.description }}</p>
 				<p class ="text-xs-right caption mb-0 ">Created at: {{ room.createdAt }}</p>
 			</v-card-text>
+		</router-link>
 	</v-card>
-
 
 </template>
 
@@ -22,7 +23,25 @@ import session from 'vue-cookie'
 
 export default {
 	name: 'room-card',
-	props: [ 'room' ],
+	props: [ 'room', 'userId' ],
+	methods: {
+
+	}
 }
 
 </script>
+
+<style scoped>
+	a:link {
+		text-decoration: none;
+	}
+	a:visited{
+		text-decoration: none;
+	}
+	a:hover{
+		text-decoration: none;
+	}
+	a:active{
+		text-decoration: none;
+	}
+</style>
