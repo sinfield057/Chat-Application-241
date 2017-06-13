8e7d66753c0664bd6736fec14ce7c2d6b4c6f388
Ionut-Catalin Tatuta <cata_lee_yn@yahoo.com>
2017-06-13 23:50:07 +0300

    Adds MessageBubble subcomponent
    https://trello.com/c/vOGwq1xt/24-create-messagebubble-subcomponent-for-chat-window

---
+++ chat_app_fe/src/components/subcomponents/MessageBubble.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card :class="messageType">
+    <v-card-title class="pa-0">
+      <span class="text-xs-left title ml-2">{{sender}}</span>
+    </v-card-title>
+    <p class ="text-xs-left subheading px-4 mb-0">
+      <slot>Mesaj Gol</slot>
+    </p>
+    <p class ="text-xs-right caption mb-0 mr-2">Sent at: {{date}}</p>
+  </v-card>
+</template>
+<script>
+import axios from 'axios'
+import router from '../../router'
+import session from 'vue-cookie'
+
+export default {
+  name: 'message-bubble',
+  props: [ 'sender', 'receiver', 'date' ],
+  data() {
+    return {
+      sent: "message-bubble py-2 elevation-1 mb-2 grey lighten-3 sent",
+      received: "message-bubble py-2 elevation-1 mb-2 blue lighten-3 received"
+    }
+  },
+  computed: {
+      messageType: function(){
+        return this.sender === this.receiver ? this.sent: this.received;
+      }
+  }
+}
+</script>
+<style scoped>
+  .message-bubble{
+    max-width:50%;
+    margin: 0 auto;
+  }
+  .sent{
+    margin-right: 20px;
+  }
+  .received{
+    margin-left: 20px;
+  }
+</style>
